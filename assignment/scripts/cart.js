67e7c59444c8dd6e1a88a4451f63faf9afd78d4f
Ivona Olvera-Radenovic <ivonaolvera@gmail.com>
2024-04-10 10:48:55 -0700

    changes

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,15 +2,32 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+``
+// does it need declaration
+let basket = [];
 
+function addItem(item) {
+    basket.push(item);
+    // if no return function might show undefined
+    return true;
+}
+console.log('added bread', addItem('bread'));
+console.log(basket);
+console.log('added milk', addItem('milk'));
+console.log('updated basket', basket);
 
+function listItems(){
+    for(let items of basket){
+     console.log('individual item', items)
+    }
+}
+listItems();
 
-
-
-
-
-
-
+function empty() {
+    basket.length = 0;
+}
+empty();
+console.log(basket);
 
 // DO NOT MODIFY
 // Used for automated testing
